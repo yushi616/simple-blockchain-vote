e21102ab53e30645f479aa19ca4e86d58071e5d4
tzy168 <3265577395@qq.com>
2023-12-05 20:05:06 +0800

    eee eeeee

--- contracts/voteApp.sol
+++ contracts/voteApp.sol
@@ -32,6 +32,15 @@ contract Voting{
     }
 
     //投票
+    //候选项目
+    struct Candidate{//候选项目结构
+        string id;
+        string name;
+        string description;
+        uint256 _total;
+    }
+    mapping(string => Candidate) candidates;//候选项目id=>候选项目
+    
     struct Vote{//投票结构
         string id;
         string name;
@@ -39,14 +48,26 @@ contract Voting{
         uint256 startTime;
         uint256 endTime;
         uint256 total;
+        Candidate[] candidates;
     }
     mapping(string => Vote) votes;//投票id=>投票
     //创建投票
     event CreateVote(string id, string name, string description, uint256 startTime, uint256 endTime);
+    event AddCandidateToVote(string id,string candidateId);
     event RemoveVote(string id);
     event voting(string id,address addr);
     event cancelVoting(string id,address addr);
 
+    //创建候选项目
+    function createCandidate(string memory _id,string memory name, string memory description) public{
+        require(msg.sender == admin, "You are not the admin.");
+        candidates[_id].id = _id;
+        candidates[_id].name = name;
+        candidates[_id].description = description;
+        candidates[_id]._total = 0;
+    }
+    
+    //创建投票
     function createVote(string memory _id,string memory name, string memory description, uint256 endTime) public{
         require(msg.sender == admin, "You are not the admin.");
         uint256 _endTime = endTime*1 days;
@@ -58,6 +79,12 @@ contract Voting{
         votes[_id].total = 0;
         emit CreateVote(_id, name, description, block.timestamp, endTime);
     }
+    //为投票添加候选项目
+    function addCandidateToVote(string memory id,string memory candidateId) public{
+        require(msg.sender == admin, "You are not the admin.");
+        votes[id].candidates.push(candidates[candidateId]);
+        emit AddCandidateToVote(id,candidateId);
+    }
     //删除投票
     function removeVote(string memory id) public{
         require(msg.sender == admin, "You are not the admin.");
@@ -65,9 +92,10 @@ contract Voting{
         emit RemoveVote(id);
     }
     //投票
-    function vote(string memory id) public{
+    function vote(string memory id,string memory _candidateId) public{
         require(block.timestamp >= votes[id].startTime && block.timestamp <= votes[id].endTime, "The vote is not in progress.");
         votes[id].total++;
+        candidates[_candidateId]._total++;
         emit voting(id,msg.sender);
     }
     //撤销投票
@@ -77,6 +105,16 @@ contract Voting{
         emit cancelVoting(id,msg.sender);
     }
     //查询
+
+    //查询投票项目
+    function getCandidate(string memory id) public view returns(Candidate memory){
+        return candidates[id];
+    }
+    
+    function getAllCandidatesOfVote(string memory id) public view returns(Candidate[] memory){
+        return votes[id].candidates;
+    }
+
     //查询投票
     function getVote(string memory id) public view returns(Vote memory){
             return votes[id];
--- src/app.js
+++ src/app.js
@@ -1,40 +1,5 @@
-const Web3 = require('web3');
-const fs = require('fs');
-const path = require('path');
-
-// 连接到以太坊节点
-const web3 = new Web3('http://localhost:8545');
-
-// 读取ABI
-const abiPath = path.resolve(__dirname, 'VoteApp.json'); // 请替换为你的ABI文件路径
-const abi = JSON.parse(fs.readFileSync(abiPath, 'utf8'));
-
-// 合约地址
-const contractAddress = '0x...'; // 请替换为你的合约地址
-
-// 创建合约实例
-const voteApp = new web3.eth.Contract(abi, contractAddress);
-
-// 创建投票
-async function createVote(id, name, description, endTime) {
-    const accounts = await web3.eth.getAccounts();
-    const admin = accounts[0];
-    const result = await voteApp.methods.createVote(id, name, description, endTime).send({ from: admin });
-    console.log(result);
+App = {
+    web3Provider: null,
+    contracts: {},
+//vote
 }
-
-// 删除投票
-async function removeVote(id) {
-    const accounts = await web3.eth.getAccounts();
-    const admin = accounts[0];
-    const result = await voteApp.methods.removeVote(id).send({ from: admin });
-    console.log(result);
-}
-
-// 测试函数
-async function test() {
-    await createVote('1', 'test1', 'justTest1', 5);
-    await removeVote('1');
-}
-
-test();
