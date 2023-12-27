10ab5ad9e89285f6925218d84e81cb5962ed3fc2
coconal <m15196712167@163.com>
2023-12-27 16:46:05 +0800

    update

--- contracts/voteApp.sol
+++ contracts/voteApp.sol
@@ -4,6 +4,9 @@ pragma solidity ^0.8.19;
 contract Voting {
     //管理员
     address admin;
+    // 添加两个状态变量来跟踪下一个可用的候选人和投票的 ID
+    uint256 nextCandidateId = 0;
+    uint256 nextVoteId = 0;
 
     constructor(address _admin) {
         admin = _admin;
@@ -20,13 +23,12 @@ contract Voting {
     //注册
     function Sign_up() public {
         require(users[msg.sender].addr != msg.sender, "You are the user.");
-        users[msg.sender].userid = userCount + 1;
+        users[msg.sender].userid = userCount;
         users[msg.sender].addr = msg.sender;
         userCount++;
     }
 
-    //登录
-    function Log_in() public view returns (bool) {
+    function isRregiser() public view returns (bool) {
         if (users[msg.sender].addr == msg.sender) {
             return true;
         } else {
@@ -38,85 +40,84 @@ contract Voting {
     //候选项目
     struct Candidate {
         //候选项目结构
-        string id;
+        uint256 id;
         string name;
         string description;
         uint256 _total;
     }
-    mapping(string => Candidate) _candidates; //候选项目id=>候选项目
+    mapping(uint256 => Candidate) _candidates; //候选项目id=>候选项目
 
     struct Vote {
         //投票结构
-        string id;
+        uint256 id;
         string name;
         string description;
         uint256 startTime;
         uint256 endTime;
         uint256 total;
         Candidate[] candidates;
-        mapping(address => string) voteds; //已投票用户，用户地址=>候选项目id，防止重复投票，可以撤销投票，可以查看投票结果
-        mapping(address => uint256) allowVotedNum; //允许投票次数，用户地址=>允许投票次数，3次
     }
-    mapping(string => Vote) votes; //投票id=>投票
+    mapping(address => uint256[]) voteds; //已投票用户，用户地址=>候选项目id，防止重复投票，可以撤销投票，可以查看投票结果
+    mapping(uint256 => Vote) votes; //投票id=>投票
     uint256 public voteCount;
     //创建投票
     event CreateVote(
-        string id,
+        uint256 id,
         string name,
         string description,
         uint256 startTime,
         uint256 endTime
     );
-    event AddCandidateToVote(string id, string candidateId);
-    event RemoveVote(string id);
-    event voting(string id, uint256 candidatesId, address addr);
-    event cancelVoting(string id, address addr);
+    event AddCandidateToVote(uint256 id, uint256 candidateId);
+    event RemoveVote(uint256 id);
+    event voting(uint256 id, uint256 candidatesId, address addr);
+    event cancelVoting(uint256 id, address addr);
 
     //创建候选项目
     function createCandidate(
-        string memory _id,
         string memory name,
         string memory description
     ) public {
         require(msg.sender == admin, "You are not the admin.");
-        //this id alreay exist
-        require(
-            keccak256(abi.encodePacked(_candidates[_id].id)) !=
-                keccak256(abi.encodePacked(_id)),
-            "The candidate id already exists."
-        );
-        _candidates[_id].id = _id;
-        _candidates[_id].name = name;
-        _candidates[_id].description = description;
-        _candidates[_id]._total = 0;
+        _candidates[nextCandidateId].id = nextCandidateId;
+        _candidates[nextCandidateId].name = name;
+        _candidates[nextCandidateId].description = description;
+        _candidates[nextCandidateId]._total = 0;
+
+        // 增加下一个可用的候选人 ID
+        nextCandidateId++;
     }
 
     //创建投票
     function createVote(
-        string memory _id,
         string memory name,
         string memory description,
         uint256 endTime
     ) public {
         require(msg.sender == admin, "You are not the admin.");
-        //alreay exist
-        require(bytes(votes[_id].name).length == 0, "The vote already exists.");
         uint256 _endTime = endTime * 1 days;
-        votes[_id].id = _id;
-        votes[_id].name = name;
-        votes[_id].description = description;
-        votes[_id].startTime = block.timestamp;
-        votes[_id].endTime = block.timestamp + _endTime;
-        votes[_id].total = 0;
+        votes[nextVoteId].id = nextVoteId;
+        votes[nextVoteId].name = name;
+        votes[nextVoteId].description = description;
+        votes[nextVoteId].startTime = block.timestamp;
+        votes[nextVoteId].endTime = block.timestamp + _endTime;
+        votes[nextVoteId].total = 0;
         voteCount++;
-        emit CreateVote(_id, name, description, block.timestamp, endTime);
+
+        emit CreateVote(
+            nextVoteId,
+            name,
+            description,
+            block.timestamp,
+            endTime
+        );
+
+        // 增加下一个可用的投票 ID
+        nextVoteId++;
     }
 
     //为投票添加候选项目
-    function addCandidateToVote(
-        string memory id,
-        string memory candidateId
-    ) public {
+    function addCandidateToVote(uint256 id, uint256 candidateId) public {
         require(msg.sender == admin, "You are not the admin.");
         require(
             bytes(_candidates[candidateId].name).length != 0,
@@ -128,7 +129,7 @@ contract Voting {
     }
 
     //删除投票
-    function removeVote(string memory id) public {
+    function removeVote(uint256 id) public {
         require(msg.sender == admin, "You are not the admin.");
         require(bytes(votes[id].name).length != 0, "The vote does not exist.");
         delete votes[id];
@@ -137,16 +138,10 @@ contract Voting {
     }
 
     //投票
-    function vote(string memory id, uint256 _candidateId) public {
+    function vote(uint256 id, uint256 _candidateId) public {
         require(users[msg.sender].addr == msg.sender, "You are not the user.");
         require(bytes(votes[id].name).length != 0, "The vote does not exist.");
-        require(
-            keccak256(abi.encodePacked(votes[id].voteds[msg.sender])) !=
-                keccak256(
-                    abi.encodePacked(votes[id].candidates[_candidateId].id)
-                ),
-            "You have already voted for this candidate"
-        );
+        require("You have already voted for this candidate");
         require(
             block.timestamp >= votes[id].startTime &&
                 block.timestamp <= votes[id].endTime,
@@ -154,13 +149,13 @@ contract Voting {
         );
         votes[id].total++;
         votes[id].candidates[_candidateId]._total++;
-        votes[id].voteds[msg.sender] = votes[id].candidates[_candidateId].id;
+        voteds[msg.sender] = votes[id].candidates[_candidateId].id;
 
         emit voting(id, _candidateId, msg.sender);
     }
 
     //撤销投票
-    function cancelVote(string memory id) public {
+    function cancelVote(uint256 id) public {
         require(users[msg.sender].addr == msg.sender, "You are not the user.");
         require(bytes(votes[id].name).length != 0, "The vote does not exist.");
         require(
@@ -168,27 +163,21 @@ contract Voting {
                 block.timestamp <= votes[id].endTime,
             "The vote is not in progress."
         );
-        require(
-            keccak256(abi.encodePacked(votes[id].voteds[msg.sender])) !=
-                keccak256(abi.encodePacked("")),
-            "You have not voted yet."
-        );
-        votes[id].voteds[msg.sender] = "";
+        require(voteds[msg.sender] != 0, "You have not voted yet.");
+        voteds[msg.sender] = 0;
         votes[id].total--;
         emit cancelVoting(id, msg.sender);
     }
 
     //查询
-    //查询投票项目
-    function getCandidate(
-        string memory id
-    ) public view returns (Candidate memory) {
+    //查询候选项目
+    function getCandidate(uint256 id) public view returns (Candidate memory) {
         return _candidates[id];
     }
 
     //
     function getAllCandidatesOfVote(
-        string memory id
+        uint256 id
     ) public view returns (Candidate[] memory) {
         return votes[id].candidates;
     }
@@ -199,26 +188,12 @@ contract Voting {
     }
 
     //查询投票
-    function getVote(string memory id) public view returns (string memory,
-        string memory,
-        string memory,
-        uint256,
-        uint256,
-        uint256,
-        Candidate[] memory) {
-        return (
-            votes[id].id,
-            votes[id].name,
-            votes[id].description,
-            votes[id].startTime,
-            votes[id].endTime,
-            votes[id].total,
-            votes[id].candidates
-        );
+    function getVote(uint256 id) public view returns (Vote memory) {
+        return votes[id];
     }
 
     //查询投票结果
-    function getVoteResult(string memory id) public view returns (uint256) {
+    function getVoteResult(uint256 id) public view returns (uint256) {
         return votes[id].total;
     }
 
